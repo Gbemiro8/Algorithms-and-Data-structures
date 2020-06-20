694036dc66bcd2cfb1b982a22fbb17a59a782ec7
Gbemiro8 <peculiar.isrl17@gmail.com>
2020-06-20 02:28:54 +0100

    https://codeforces.com/problemset/problem/1362/C

---
+++ bitmask.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+int main()
+{
+	int t; cin >> t;
+	while(t--){
+		ll n; cin >> n;
+		ll ans = 0;
+		while(n){
+			ans += n;
+			n /= 2;
+		}
+		cout << ans << "\n";
+	}
+}
